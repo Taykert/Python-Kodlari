34c1871db29baab33654321b432bda59497ef9ee
Taykert <slhararat@gmail.com>
2025-01-05 13:52:07 +0300

    Update Sezar Şifresi Kırma.py

--- Sezar Şifresi Kırma.py
+++ Sezar Şifresi Kırma.py
@@ -1,12 +1,15 @@
 print("Sezar Şifresi Kırma Programına Hoşgeldiniz.")
 sifresiz=input("Şifresi kırılacak metni giriniz:")
-def sifrele(metin):
+def sifrele(metin):AnKArA76?:.
+def sifrele(metin):AnKArA76?:.
+def sifrele(metin):AnKArA76?:.
+def sifrele(metin):AnKArA76?:.
     sifrelimetin=""
-    for harf in metin:
-        asciikod=ord(harf)
+    for harf in metin:AKAA
+        asciikod=ord(harf)12
         asciikod=asciikod-3
-        karakterkod=chr(asciikod)
-        sifrelimetin=sifrelimetin+karakterkod
-    print("Şifresiz:",metin,"Şifreli:",sifrelimetin)
+        karakterkod=chr(asciikod)76?
+        sifrelimetin=sifrelimetin+karakterkodYASİN
+    print("Şifresiz:",metin,"Şifreli:",sifrelimetin):.
 
-sifrele(sifresiz)
+sifrele(sifresiz)şifreli
